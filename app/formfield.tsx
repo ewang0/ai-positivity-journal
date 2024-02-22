9bdf59580f2f6eccfd7210ad7631dd383a9e1888
Eric Wang <87143658+ewang0@users.noreply.github.com>
2024-02-22 16:52:15 -0500

    Add formfields

---
+++ app/formfield.tsx
@@ -0,0 +1,23 @@
+interface FormFieldProps {
+    header: string;
+    subheader: string;
+    placeholder: string;
+}
+
+const FormField: React.FC<FormFieldProps> = ({ header, subheader, placeholder}) => {
+    return (
+        <div className="w-full mt-8">
+            <label className="block text-white text-lg font-bold mb-2" htmlFor="username">
+                {header}
+            </label>
+            <textarea 
+                className="bg-white text-gray-700 w-full border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white resize-none" 
+                id="grid-first-name" 
+                rows={3}
+                placeholder={placeholder}
+            />
+        </div>
+    );
+}
+
+export default FormField;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
-
+import FormField from "./formfield";
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
+    <main className="flex flex-col justify-center items-center max-w-[565px] m-auto">
       <div className="flex flex-col justify-center items-center py-16 border-b-2">
         {/* <img src="vercel.svg" className="bg-white"/> */}
         <h1 className="font-mono font-bold mb-5 text-center text-7xl">AI Naming App</h1>
         <p className="font-sans max-w-sm text-center">Built with Next.js, Tailwind CSS, TanStack Query, AWS Lambda, PlanetScale DB, and OpenAI</p>
       </div>
-
+      <FormField 
+        header={"What are you looking to name?"} 
+        subheader={"butt"} 
+        placeholder="Ex: “A composable platform that integrates all of a patient's data to enable more powerful medicine, today and tomorrow”"
+      />
+      <FormField 
+        header={"Themes you'd like to explore:"} 
+        subheader={"butt"} 
+        placeholder="Ex: “Full picture, Freedom to focus, A vibrant future, Human perspective, etc.”"
+      />
+      <FormField 
+        header={"Related words:"} 
+        subheader={"butt"} 
+        placeholder="Ex: “Innovation, Medicine, Technology, Assisted, Doctor, etc”"
+      />
     </main>
   );
 }

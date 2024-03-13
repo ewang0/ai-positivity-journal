1cfd7cbeeb6519bc5291039dd73615a09048b986
Eric Wang <87143658+ewang0@users.noreply.github.com>
2024-03-13 16:39:14 -0400

    Styling updates

--- app/icons.tsx
+++ app/icons.tsx
@@ -6,7 +6,7 @@ export const VercelIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#000000" />
+      <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#FFFFFF" />
     </svg>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import History from "./history";
 import { useState, useRef } from "react";
 import { useChat, Message } from "ai/react";
-import { LoadingCircle } from "./icons";
+import { LoadingCircle, VercelIcon } from "./icons";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { STRINGS } from "./strings.js"
@@ -50,90 +50,96 @@ export default function Home() {
   );
 
   return (
-    <main className="flex flex-col max-w-[880px] m-auto">
-      <div className="flex flex-col justify-center items-center w-full py-12 border-b-2">
-        <h1 className="font-mono font-bold mb-5 text-center text-5xl">AI Positivity Journal</h1>
-        <p className="font-sans max-w-sm text-center">Built with Next.js, Tailwind CSS, PlanetScale DB, OpenAI streaming, and Vercel AI SDK</p>
-      </div>
-      <div className="block text-white text-lg font-bold mb-10 mt-8 max-w-3xl">
-        {STRINGS.INTRO}
-      </div>
-      <div className="flex flex-col">
-        <div className="block text-white text-lg font-bold mb-4 mt-8 max-w-3xl">
-          Try some examples:
+    <main className="px-16">
+      <div className="flex flex-col max-w-[880px] m-auto">
+        <div className="flex flex-col justify-center items-center w-full py-12 border-b-2">
+          <h1 className="font-mono font-bold mb-5 text-center text-5xl">AI Positivity Journal</h1>
+          <p className="font-sans max-w-sm text-center">Built with Next.js, Tailwind CSS, PlanetScale DB, OpenAI streaming, and Vercel AI SDK</p>
         </div>
-        <div className="grid grid-cols-3 gap-4">
-          {STRINGS.EXAMPLES.map((example, i) => (
-            <button 
-              key={i} 
-              className="rounded-md border border-black text-center text-md bg-stone-700 px-5 py-5 text-left transition-all duration-75 hover:border-white hover:bg-stone-600 active:bg-gray-50 cursor-pointer"
-              onClick={() => {
-                setInput(example.content);
-                inputRef.current?.focus();
-              }}
-            >
-              {example.title}
-            </button>)
-          )}
+        <div className="block text-white text-lg font-bold mb-16 mt-8 max-w-3xl">
+          {STRINGS.INTRO}
         </div>
-      </div>
-      <div>
-        <form 
-          onSubmit={handleSubmit} 
-          className="w-full mt-8"
-        >
-            <label className="block text-white text-lg font-bold mb-3" htmlFor="username">
-              Describe your negative life event here:
-            </label>
-            <textarea 
-                ref={inputRef}
-                className="bg-white text-gray-700 w-full border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white resize-none" 
-                id="grid-first-name" 
-                rows={8}
-                placeholder={"Ex: “This morning, my car wouldn't start, making me late for an important meeting. As I waited for the tow truck, frustration and anxiety washed over me. It seemed like the day was set to be a disaster from the start.”"}
-                value={input}
-                required
-                onChange={(e) => setInput(e.target.value)}
-            />
-          <button onClick={() => setOriginalInput(input)} className="flex justify-center items-center bg-blue-500 w-full hover:bg-blue-400 mt-3 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            {isLoading ? ButtonLoading : "Generate Positivity"}
-          </button>
-        </form>
-      </div>
-      <div className='grid grid-cols-2 gap-x-8'>
-        <div className="flex flex-col w-full pt-8">
-          <div className="block text-white text-lg font-bold mb-3">Original Entry:</div>
-          <div className="h-[400px] max-h-[400px] overflow-scroll bg-stone-800 rounded py-3 px-4">
-            {originalInput}
+        <div className="flex flex-col">
+          <div className="block text-white text-lg font-bold mb-4 max-w-3xl">
+            Try some examples:
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            {STRINGS.EXAMPLES.map((example, i) => (
+              <button 
+                key={i} 
+                className="rounded-md border border-black text-center text-md bg-zinc-700 px-5 py-5 text-left transition-all duration-75 hover:border-white hover:bg-zinc-600 active:bg-gray-50 cursor-pointer"
+                onClick={() => {
+                  setInput(example.content);
+                  inputRef.current?.focus();
+                }}
+              >
+                {example.title}
+              </button>)
+            )}
+          </div>
+        </div>
+        <div>
+          <form 
+            onSubmit={handleSubmit} 
+            className="w-full mt-9"
+          >
+              <label className="block text-white text-lg font-bold mb-3" htmlFor="username">
+                Describe your negative life event here:
+              </label>
+              <textarea 
+                  ref={inputRef}
+                  className="bg-white text-gray-700 w-full border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white resize-none" 
+                  id="grid-first-name" 
+                  rows={8}
+                  placeholder={"Ex: “This morning, my car wouldn't start, making me late for an important meeting. As I waited for the tow truck, frustration and anxiety washed over me. It seemed like the day was set to be a disaster from the start.”"}
+                  value={input}
+                  required
+                  onChange={(e) => setInput(e.target.value)}
+              />
+            <button onClick={() => setOriginalInput(input)} className="flex justify-center items-center bg-blue-500 w-full hover:bg-blue-400 mt-3 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+              {isLoading ? ButtonLoading : "Generate Positivity"}
+            </button>
+          </form>
+        </div>
+        <div className='grid grid-cols-2 gap-x-8'>
+          <div className="flex flex-col w-full pt-9">
+            <div className="block text-white text-lg font-bold mb-3">Original Entry:</div>
+            <div className="h-[400px] max-h-[400px] overflow-scroll bg-zinc-800 rounded py-3 px-4">
+              {originalInput}
+            </div>
+          </div>
+          <div className="flex flex-col w-full pt-9">
+            <div className="block text-white text-lg font-bold mb-3">Generated Positivity:</div>
+            <div className="h-full max-h-[400px] overflow-scroll bg-zinc-800 rounded py-3 px-4">
+              {messages[messages.length - 1]?.role === "user" ? <LoadingCircle/> : 
+                <ReactMarkdown
+                    className="prose mt-1 w-full break-words prose-p:leading-relaxed"
+                    remarkPlugins={[remarkGfm]}
+                    components={{
+                      // open links in new tab
+                      a: (props) => (
+                        <a {...props} target="_blank" rel="noopener noreferrer" />
+                      ),
+                    }}
+                  >
+                    {messages[messages.length - 1]?.role === "assistant" ? messages[messages.length - 1]?.content : ''}
+                </ReactMarkdown>
+              }
+            </div>
+            {/* <button className="bg-blue-500 w-full hover:bg-blue-400 mt-[18px] text-white font-bold py-2 px-4 border-b-4 border-blue-700 opacity-50 cursor-not-allowed rounded">
+              Save
+            </button> */}
           </div>
           {/* <button className="bg-blue-500 w-full hover:bg-blue-400 mt-[18px] text-white font-bold py-2 px-4 border-b-4 border-blue-700 opacity-50 cursor-not-allowed rounded">
-            Save
+            Save Results
           </button> */}
         </div>
-        <div className="flex flex-col w-full pt-8">
-          <div className="block text-white text-lg font-bold mb-3">Generated Positivity:</div>
-          <div className="h-full max-h-[400px] overflow-scroll bg-stone-800 rounded py-3 px-4">
-            {messages[messages.length - 1]?.role === "user" ? <LoadingCircle/> : 
-              <ReactMarkdown
-                  className="prose mt-1 w-full break-words prose-p:leading-relaxed"
-                  remarkPlugins={[remarkGfm]}
-                  components={{
-                    // open links in new tab
-                    a: (props) => (
-                      <a {...props} target="_blank" rel="noopener noreferrer" />
-                    ),
-                  }}
-                >
-                  {messages[messages.length - 1]?.role === "assistant" ? messages[messages.length - 1]?.content : ''}
-              </ReactMarkdown>
-            }
-          </div>
-          {/* <button className="bg-blue-500 w-full hover:bg-blue-400 mt-[18px] text-white font-bold py-2 px-4 border-b-4 border-blue-700 opacity-50 cursor-not-allowed rounded">
-            Save
-          </button> */}
+        {/* <History /> */}
+        <div className="flex justify-center items-center gap-4 mb-12 mt-16">
+          <p>Deployed with Vercel</p>
+          <VercelIcon />
         </div>
       </div>
-      <History />
     </main>
   );
 }
